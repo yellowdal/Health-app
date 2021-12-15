aa6dca69e363faa741427b262787901543a4e622
Uddeshya Jha <62802044+yellowdal@users.noreply.github.com>
2021-12-16 00:27:03 +0530

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\health-chatt-bot-999e-Web-Deploy.pubxml -p:Password=4jbkhzl3Ek4QoJ9Ccopl56wERFjC7b1haolDY0p2gjXyijXZTi8SjP7u43E1
+
